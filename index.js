994a5797d67e8dc7bffa86ed5efd7754619c7828
nicoIosa <nicolasiosa@hotmail.com>
2024-03-05 11:35:40 -0300

    configuramos middlewares y una primera ruta

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-import express from "express"
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import "dotenv/config"; // permite procesar variables de entorno
+
 // node --watch index.js (comando experimental para desarrollo; run dev)
 console.log("Bienvenidos c74i");
+
+// 1- Confgurar un puerto
+const app = express();
+
+app.set("port", process.env.PORT || 4000);
+app.listen(app.get("port"), () => {
+  console.log("Estoy en el puerto " + app.get("port"));
+});
+
+// 2- Configurar middlewares
+app.use(cors()); // permite conexiones/solicitudes remotas
+app.use(morgan("dev")); // muestra informacion extra en la terminal
+app.use(express.json()); // permite interpretar el formato json
+app.use(express.urlencoded({ extended: true })); // permite interpretar los datos del body de un request
+
+// 3- Configuracion de las rutas
+app.get("/", (req, res) => {
+  console.log("Hola mundo");
+  res.send("Desde el backend de RollingCoffee");
+});
