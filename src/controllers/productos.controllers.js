4a6bf081268600986ee568ea4efce6f0d3831ba9
nicoIosa <nicolasiosa@hotmail.com>
2024-03-14 11:21:02 -0300

    agregamos la funcion editar

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -44,3 +44,25 @@ export const obtenerProducto = async (req, res) => {
     });
   }
 };
+export const editarProducto = async (req, res) => {
+  try {
+    // extraer el id y los datos del producto a modificar del body
+    // buscar si encontramos el producto con el id
+    const productoBuscado = await Producto.findById(req.params.id);
+    // enviar un mensaje de error en caso de no encontrar el producto
+    if (!productoBuscado) {
+      return res.status(404).json({
+        message: "No se encontro el producto buscado",
+      });
+    }
+    // editar el producto
+    await Producto.findByIdAndUpdate(req.params.id, req.body);
+    // contestamos al front con un status 200
+    res.status(200).json({ message: "El producto fue editado correctamente" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Ocurrio un error al editar el producto",
+    });
+  }
+};
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   crearProducto,
+  editarProducto,
   listarProductos,
   obtenerProducto,
 } from "../controllers/productos.controllers.js";
@@ -8,6 +9,6 @@ import {
 const router = Router();
 
 router.route("/productos").get(listarProductos).post(crearProducto);
-router.route("/productos/:id").get(obtenerProducto);
+router.route("/productos/:id").get(obtenerProducto).put(editarProducto);
 
 export default router;
